1202e1b7e1f579feb918cdf966cad3790d009179
Omer Khan <okhanlodhi@gmail.com>
2021-04-21 00:34:40 -0500

    Getting a flask app up and running on basic html template

--- Website/__init__.py
+++ Website/__init__.py
@@ -4,5 +4,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'u\xbb\xa3\x08\x12\xbc"\xf9\xd9X\\\x02\x9d\xebk^\x14\x98b\xa6YUl\x1a'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth/')
+
     return app
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Website import create_app
+from Website import templates
 
 app = create_app()
 if __name__ == '__main__':
